f6dcc9ca81dbd4b77246fdc6239c4a2336fba212
Revashan <141326969+Revashan@users.noreply.github.com>
2026-04-09 13:11:00 +0800

    Update Repeat-customers.sql

--- sql/Repeat-customers.sql
+++ sql/Repeat-customers.sql
@@ -1,7 +1,1 @@
-/* Repeated customers segregation*/
 
-SELECT customer_unique_id, COUNT(order_id) AS total_orders
-FROM ecommerce_data
-GROUP BY customer_unique_id
-HAVING COUNT(order_id) > 1
-ORDER BY total_orders DESC;
